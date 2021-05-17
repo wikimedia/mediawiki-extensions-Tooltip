59104bf344faac9e3421845abe90d48e06ff81dd
zoranzoki21 <zorandori4444@gmail.com>
2021-05-17 07:33:08 +0200

    Convert Tooltip to use extension registration
    Bug: T282988
    Change-Id: I76cd48c55b5c41b566193bed9c35e9f3c9da8388

--- Tooltip.php
+++ Tooltip.php
@@ -1,75 +1,16 @@
 <?php
 
-$wgExtensionCredits['parserhook'][] = array(
-	'path'           => __FILE__,
-	'name'           => 'ToolTip',
-	'author'         => 'Paul Grinberg',
-	'url'            => 'https://www.mediawiki.org/wiki/Extension:Tooltip',
-	'descriptionmsg' => 'tooltip-desc',
-	'version'        => '0.6.1',
-);
-
-$dir = dirname( __FILE__ ) . '/';
-$wgMessagesDirs['Tooltip'] = $dir . 'i18n';
-$wgExtensionMessagesFiles['TooltipMagic'] = $dir . 'Tooltip.i18n.magic.php';
-
-$wgHooks['ParserFirstCallInit'][] = 'wfToolTipRegisterParserHooks';
-$wgHooks['BeforePageDisplay'][] = 'wfTooltipBeforePageDisplay';
-
-$wgResourceModules += array(
-	'ext.tooltip' => [
-		'scripts' => 'Tooltip.js',
-		'styles' => 'Tooltip.css',
-		'localBasePath' => __DIR__ . '/modules',
-		'remoteExtPath' => 'Tooltip/modules',
-] );
-
-function wfToolTipRegisterParserHooks( $parser ) {
-    $parser->setHook( 'tooltip', 'renderToolTip' );
-    $parser->setFunctionHook( 'tooltip', 'wfTooltipParserFunction_Render' );
-    return true;
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'Tooltip' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['Tooltip'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['TooltipMagic'] = __DIR__ . '/Tooltip.i18n.magic.php';
+	wfWarn(
+		'Deprecated PHP entry point used for the Tooltip extension. ' .
+		'Please use wfLoadExtension() instead, ' .
+		'see https://www.mediawiki.org/wiki/Special:MyLanguage/Manual:Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the Tooltip extension requires MediaWiki 1.35+' );
 }
-
-function wfTooltipBeforePageDisplay( $out ) {
-	$out->addModules( 'ext.tooltip' );
-	return true;
-}
-
-function wfTooltipParserFunction_Render( $parser, $basetext = '', $tooltiptext = '', $x = 0, $y = 0) {
-    $output = renderToolTip($tooltiptext, array('text'=>$basetext, 'x'=>$x, 'y'=>$y), $parser);
-    return array($output, 'nowiki' => false, 'noparse' => true, 'isHTML' => false);
-}
-
-function renderToolTip($input, $argv, &$parser) {
-    $text = 'see tooltip';
-    $xoffset = 0;
-    $yoffset = 0;
-    foreach ($argv as $key => $value) {
-        switch ($key) {
-            case 'text':
-                $text = $value;
-                break;
-            case 'x':
-                $xoffset = intval($value);
-                break;
-            case 'y':
-                $yoffset = intval($value);
-                break;
-            default :
-                wfDebug( __METHOD__ . ": Requested '$key ==> $value'\n" );
-                break;
-        }
-    }
-
-    $output = '';
-
-    if ($input != '') {
-        $tooltipid = uniqid( 'tooltipid' );
-        $parentid = uniqid( 'parentid' );
-        $output .= "<span id='$tooltipid' class='xstooltip'>" . $parser->unstrip($parser->recursiveTagParse($input),$parser->mStripState) . "</span>";
-        $output .= "<span id='$parentid' class='xstooltip_body' onmouseover=\"xstooltip_show('$tooltipid', '$parentid', $xoffset, $yoffset);\" onmouseout=\"xstooltip_hide('$tooltipid');\">" . $parser->unstrip($parser->recursiveTagParse($text),$parser->mStripState) . "</span>";
-    }
-
-    return $output;
-}
-
